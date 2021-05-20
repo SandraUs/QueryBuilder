e4a4e1dfa73423d54bc17cfc97a51f5040f5c5f1
SandraUs <66241283+SandraUs@users.noreply.github.com>
2021-05-20 09:02:43 +0700

    Add files via upload

---
+++ classes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	"database" => [
+		"database" => "app3",
+		"username" => "root",
+		"password" => "mysql",
+		"connection" => "mysql:host=localhost",
+		"charset" => "utf8"
+
+	],
+
+];
